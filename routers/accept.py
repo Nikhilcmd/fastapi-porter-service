1280dd4116f7ec106631b4bf6dbfc156a4db6fb3
Nikhilcmd <sharmanikhil03024@gmail.com>
2026-04-30 06:13:15 +0530

    fix: Accept endpoint logic update

--- routers/accept.py
+++ routers/accept.py
@@ -16,16 +16,19 @@ def accept(req_id:int,user: str=Depends(get_current_user),db:Session=Depends(get
     if Role(user["role"])== Role.DRIVER:
         try:
             driver=db.query(Driver).filter(Driver.account_id==user["id"]).one()
+            driver_eligible=db.query(porter).filter(porter.driver_id==driver.id, or_(porter.status==Status.ACCEPTED , porter.status==Status.STARTED, porter.status==Status.COLLECTED,porter.status==Status.REACHED)).one_or_none()
+            if driver_eligible != None:
+                raise HTTPException(status_code=403,detail="You are not allowed to take up this request.")
             req=db.query(porter).filter(req_id==porter.id).with_for_update().one()
             
         except NoResultFound:
             raise HTTPException(status_code=404,detail="The request is not found.")
         except MultipleResultsFound:
             raise HTTPException(status_code=500,detail="Internal Server issue.")
-        driver_eligible=db.query(porter).filter(porter.driver_id==driver.id, or_(porter.status==Status.ACCEPTED , porter.status==Status.STARTED, porter.status==Status.COLLECTED,porter.status==Status.REACHED)).one_or_none()
         
-        if driver_eligible != None:
-            raise HTTPException(status_code=403,detail="You are not allowed to take up this request.")
+        
+        
+            
         if Status(req.status)== Status.REQUESTED:
             req.driver_id=driver.id
             req.status=Status.ACCEPTED
@@ -33,7 +36,7 @@ def accept(req_id:int,user: str=Depends(get_current_user),db:Session=Depends(get
             db.commit()
             return "Accepted wow"
         else:
-            raise HTTPException(status_code=401,detail="This request can not be accepted.")
+            raise HTTPException(status_code=409,detail="This request can not be accepted.")
 
         
 
