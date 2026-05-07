1b5e851a9cef5f4b693c3360871f05fc5170fd1e
Nikhilcmd <sharmanikhil03024@gmail.com>
2026-05-07 06:45:06 +0530

    fix: implemented redis pipeline

--- tasks.py
+++ tasks.py
@@ -4,9 +4,12 @@ from Schema import Driver, porter
 from database import Sessionlocal, r
 from enums import Status
 from datetime import datetime, UTC, timedelta
-
 import os
+import logging
 load_dotenv()
+
+logger=logging.getLogger("tasks")
+logging.basicConfig(level=logging.DEBUG)
 
 app=Celery('task',broker=os.getenv("CELERY_URL"))
 
@@ -26,38 +29,50 @@ def del_user(driver_id):
 @app.task(bind=True)
 def radius_expansion(self,request_id):
    session = Sessionlocal()
+   p=r.pipeline(transaction=False)
    
    try:
        element=session.query(porter).filter(porter.id==request_id).one_or_none()
        if element is None:
         return "Something went wrong"
-       if Status(element.status)!=Status.REQUESTED:
-          return "This ride is already accepted."
+       if element.status!=Status.REQUESTED:
+          return "This ride is already accepted or cancelled."
        diffrence=datetime.now(tz=UTC).replace(tzinfo=None)-element.requested_at
-       print(element.requested_at)
-       print("This is the diffrence",diffrence)
+       logger.debug(element.requested_at)
+       logger.debug(f"This is the diffrence{diffrence}")
+
        if diffrence > timedelta(seconds=600):
-          print("diffrence is what the hell ", diffrence.total_seconds())
+          logger.debug(f"diffrence is what the hell {diffrence.total_seconds()}")
           element.status=Status.CANCELLED
           element.cancelled_at=datetime.now(tz=UTC)
           session.commit()
           return "This ride is cancelled as no driver found."
+       cords=[]
        ride_lat=element.pickup_loc_lat
        ride_long=element.pickup_loc_long
        n=r.incr(f"radius:{request_id}",amount=1)
+       r.expire(f"radius:{request_id}",time=1020,nx=True)
        
        vri=r.geosearch("driver_location",None,ride_long,ride_lat,"km",min(n+1,9),sort="ASC",withdist=True)
        
+    
        
-       print(vri)
-       print("This is the radius",min(n+1,9))
-
+   
+       logger.debug(f"why are these here{vri}")
+       logger.debug(f"This is the radius{min(n+1,9)}")
        for t in vri:
-          if r.get(f"driver_online:{t[0]}") is not None:
-             print("this is", t[0])
-             r.zadd(f"eligible_drivers:{request_id}",{t[0]:t[1]},nx=True)
+          p.get(f"driver_online:{t[0]}")
+          cords.append(t[1])
+
+       driverid=p.execute()
+       logger.debug(type(driverid))
+
+
+       for driverid, cords in zip(driverid, cords):
+         logger.debug(f"this is{driverid}:{cords}")
+         if driverid is not None:
+            r.zadd(f"eligible_drivers:{request_id}",{driverid:cords},nx=True)
        r.expire(f"eligible_drivers:{request_id}",time=60)
-       radius_expansion.apply_async([request_id],countdown=30)
-             
+       radius_expansion.apply_async([request_id],countdown=30)        
    finally:
       session.close()
